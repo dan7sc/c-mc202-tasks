8f2f72a4d5ff297a50bbf978a14a7cfed98b20fc
dan7sc <dan7sc@users.noreply.github.com>
2020-09-22 08:26:18 -0300

    adiciona funcao le_matriz e imprime_matriz

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+#define MATRIZ_LIN 1000
+#define MATRIZ_COL 10
+
+void le_matriz(int lin, int col,  int matriz[MATRIZ_LIN][MATRIZ_COL]) {
+    for(int i = 0; i < lin; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void imprime_matriz(int lin, int col, int matriz[MATRIZ_LIN][MATRIZ_COL]) {
+    for(int i = 0; i < lin; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
-    printf("Exercicio da tarefa 02\n");
+    int lin = 6;
+    int col = 10;
+    int matriz[MATRIZ_LIN][MATRIZ_COL];
+
+    le_matriz(lin, col, matriz);
+    imprime_matriz(lin, col, matriz);
 
     return 0;
 }
