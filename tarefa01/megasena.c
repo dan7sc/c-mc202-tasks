d8d78914bd092eb35049af4392b9aeb5cf7567d9
dan7sc <dan7sc@users.noreply.github.com>
2020-09-22 14:47:42 -0300

    adiciona funcao mostra_premios_recebidos, distribui_premios e conta_premiados

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+#define NUM_APOSTADORES 1000
 #define VETOR_SIZE 6
-#define MATRIZ_LIN 1000
+#define MATRIZ_LIN 6000
 #define MATRIZ_COL 10
 
 void le_vetor(int size, int vetor[VETOR_SIZE]) {
@@ -68,13 +69,76 @@ int compara_elementos_vetor_e_matriz
     return numero_de_elementos_iguais;
 }
 
+void conta_premiados
+(
+ int numeros_sorteados,
+ int quantidade_de_premiados[]
+) {
+    if(numeros_sorteados == 6) {
+        ++quantidade_de_premiados[0];
+    } else if(numeros_sorteados == 5) {
+        ++quantidade_de_premiados[1];
+    } else if(numeros_sorteados == 4) {
+        ++quantidade_de_premiados[2];
+    }
+}
+
+void distribui_premios
+(
+ int numero_de_apostadores,
+ double premios_possiveis[],
+ int numeros_sorteados_por_apostador[],
+ int quantidade_de_premiados[]
+ ) {
+    for(int i = 0; i < numero_de_apostadores; i++) {
+        if(numeros_sorteados_por_apostador[i] == 6) {
+            printf("%.2lf\n", premios_possiveis[0] / quantidade_de_premiados[0]);
+        } else if(numeros_sorteados_por_apostador[i] == 5) {
+            printf("%.2lf\n", premios_possiveis[1] / quantidade_de_premiados[1]);
+        } else if(numeros_sorteados_por_apostador[i] == 4) {
+            printf("%.2lf\n", premios_possiveis[2] / quantidade_de_premiados[2]);
+        } else {
+            printf("%.2lf\n", premios_possiveis[3]);
+        }
+    }
+}
+
+void mostra_premios_recebidos
+(
+ int numero_de_apostadores, float premio,
+ int vsize, int numeros_sorteados[VETOR_SIZE],
+ int mlin, int mcol, int volante[MATRIZ_LIN][MATRIZ_COL]
+ ) {
+    int inicio_zero = 0;
+    int numeros_sorteados_por_apostador[NUM_APOSTADORES];
+    int quantidade_de_premiados[] = { 0, 0, 0 };
+    double premios_possiveis[] = { 0.62*premio, 0.19*premio, 0.19*premio, 0.00 };
+
+    for(int i = 0; i < numero_de_apostadores; i++) {
+        numeros_sorteados_por_apostador[i] = compara_elementos_vetor_e_matriz
+            (vsize, numeros_sorteados, mlin, mcol, volante, inicio_zero);
+
+        inicio_zero += 6;
+
+        conta_premiados
+            (numeros_sorteados_por_apostador[i],
+             quantidade_de_premiados);
+    }
+
+    distribui_premios
+        (numero_de_apostadores,
+         premios_possiveis,
+         numeros_sorteados_por_apostador,
+         quantidade_de_premiados);
+}
+
 int main() {
     int numero_de_apostadores;
-    float premio_total;
+    double premio_total;
     int numeros_sorteados[6];
 
     scanf("%d", &numero_de_apostadores);
-    scanf("%f", &premio_total);
+    scanf("%lf", &premio_total);
 
     int vsize = 6;
     int mcol = 10;
@@ -84,19 +148,12 @@ int main() {
     le_matriz(mlin, mcol, volante);
     le_vetor(vsize, numeros_sorteados);
 
-    printf("%d %lf\n", numero_de_apostadores, premio_total);
-    imprime_matriz(mlin, mcol, volante);
-    imprime_vetor(vsize, numeros_sorteados);
+    /* printf("%d %lf\n", numero_de_apostadores, premio_total); */
+    /* imprime_matriz(mlin, mcol, volante); */
+    /* imprime_vetor(vsize, numeros_sorteados); */
 
-    int inicio_do_volante = 0;
-    int cont = 0;
-
-    for(int i = 0; i < numero_de_apostadores; i++) {
-        cont = compara_elementos_vetor_e_matriz
-            (vsize, numeros_sorteados, mlin, mcol, volante, inicio_do_volante);
-        inicio_do_volante += 6;
-        printf("cont: %d\n", cont);
-    }
+    mostra_premios_recebidos
+        (numero_de_apostadores, premio_total, vsize, numeros_sorteados, mlin, mcol, volante);
 
     return 0;
 }
