674fd717b23f502d74fe30926418b33825d669ab
dan7sc <dan7sc@users.noreply.github.com>
2020-09-22 09:50:47 -0300

    adiciona funcao compara_elementos_vetor_e_matriz

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -34,18 +34,69 @@ void imprime_matriz(int lin, int col, int matriz[MATRIZ_LIN][MATRIZ_COL]) {
     }
 }
 
+int compara_elementos_vetor_e_matriz
+(
+ int vsize, int vector[VETOR_SIZE],
+ int mlin, int mcol, int matriz[MATRIZ_LIN][MATRIZ_COL],
+ int indice_zero
+ ) {
+    int indice = 0;
+    int numero_de_elementos_iguais = 0;
+
+    for(int i = 0; i < vsize; i++) {
+        indice = vector[i];
+
+        // Obtem indice da coluna na matriz
+        if(indice > 10) {
+            if(indice % 10 == 0) {
+                mcol = 9;
+            } else {
+                mcol = (indice % 10) - 1;
+            }
+        } else {
+            mcol = indice - 1;
+        }
+
+        // Obtem indice da linha na matriz
+        mlin = (indice - 1) / 10;
+
+        if(matriz[mlin + indice_zero][mcol] == 1) {
+            numero_de_elementos_iguais++;
+        }
+    }
+
+    return numero_de_elementos_iguais;
+}
+
 int main() {
-    int mlin = 6;
-    int mcol = 10;
+    int numero_de_apostadores;
+    float premio_total;
+    int numeros_sorteados[6];
+
+    scanf("%d", &numero_de_apostadores);
+    scanf("%f", &premio_total);
+
     int vsize = 6;
-    int matriz[MATRIZ_LIN][MATRIZ_COL];
-    int vetor[VETOR_SIZE];
+    int mcol = 10;
+    int mlin = numero_de_apostadores * 6;
+    int volante[MATRIZ_LIN][MATRIZ_COL];
 
-    le_matriz(mlin, mcol, matriz);
-    le_vetor(vsize, vetor);
+    le_matriz(mlin, mcol, volante);
+    le_vetor(vsize, numeros_sorteados);
 
-    imprime_matriz(mlin, mcol, matriz);
-    imprime_vetor(vsize, vetor);
+    printf("%d %lf\n", numero_de_apostadores, premio_total);
+    imprime_matriz(mlin, mcol, volante);
+    imprime_vetor(vsize, numeros_sorteados);
+
+    int inicio_do_volante = 0;
+    int cont = 0;
+
+    for(int i = 0; i < numero_de_apostadores; i++) {
+        cont = compara_elementos_vetor_e_matriz
+            (vsize, numeros_sorteados, mlin, mcol, volante, inicio_do_volante);
+        inicio_do_volante += 6;
+        printf("cont: %d\n", cont);
+    }
 
     return 0;
 }
