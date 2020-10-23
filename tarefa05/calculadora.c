a6b930d4bc59e80f114004db8dc898b11a16864b
dan7sc <dan7sc@users.noreply.github.com>
2020-10-23 13:55:13 +0000

    adiciona funcoes de leitura e conversao

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM_STR 25
+
 typedef struct No {
     int dado;
     struct No *prox;
 } No;
 
 typedef struct No * PNo;
+
+void le_int(int *num) {
+    scanf("%d", num);
+}
+
+void le_char(char *ch) {
+    scanf(" %c", ch);
+}
+
+void le_string(char *str) {
+    scanf("%s", str);
+}
 
 PNo cria_lista() {
     return NULL;
@@ -24,7 +38,7 @@ void imprime_lista(PNo lista) {
 
     atual = lista;
     while(atual != NULL) {
-        printf("%d ", atual->dado);
+        printf("%d", atual->dado);
         atual = atual->prox;
     }
     printf("\n");
@@ -61,17 +75,54 @@ PNo adiciona_elemento_no_fim(PNo lista, int dado) {
     return lista;
 }
 
-int main() {
-    PNo lista;
+int converte_char_para_int(char c) {
+    return c - '0';
+}
 
-    lista = cria_lista();
+PNo converte_string_para_lista_int(char *str_numero) {
+    int num;
+    PNo numero;
 
-    for(int i = 1; i < 10; i++) {
-        lista = adiciona_elemento_no_fim(lista, i);
+    numero = cria_lista();
+
+    for(int i = 0; str_numero[i] != '\0'; i++) {
+        num = converte_char_para_int(str_numero[i]);
+        numero = adiciona_elemento_no_fim(numero, num);
     }
 
-    imprime_lista(lista);
-    destroi_lista(lista);
+    return numero;
+}
+
+int main() {
+    int qtd_teste;
+    char operacao;
+    char str_numero1[TAM_STR];
+    char str_numero2[TAM_STR];
+    PNo numero1;
+    PNo numero2;
+
+    numero1 = cria_lista();
+    numero2 = cria_lista();
+
+    le_int(&qtd_teste);
+    le_char(&operacao);
+    le_string(str_numero1);
+    le_string(str_numero2);
+
+    numero1 = converte_string_para_lista_int(str_numero1);
+    numero2 = converte_string_para_lista_int(str_numero2);
+
+    printf("%d ", qtd_teste);
+    printf("%c\n", operacao);
+
+    /* printf("%s\n", str_numero1); */
+    /* printf("%s\n", str_numero2); */
+
+    imprime_lista(numero1);
+    imprime_lista(numero2);
+
+    destroi_lista(numero1);
+    destroi_lista(numero2);
 
     return 0;
 }
