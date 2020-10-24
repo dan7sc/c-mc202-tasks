d1285fb0104fded6f354ba04e20aa863cacc53e6
dan7sc <dan7sc@users.noreply.github.com>
2020-10-24 00:26:50 +0000

    cria funcao para operacao de soma

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -43,7 +43,7 @@ PLista cria_lista() {
     return lista;
 }
 
-Lista inicia() {
+Lista cria_resultado() {
     Lista lista;
 
     lista.inicio = NULL;
@@ -135,67 +135,110 @@ PLista converte_string_para_lista_int(PLista numero, char *str_numero) {
 
     for(int i = 0; str_numero[i] != '\0'; i++) {
         num = converte_char_para_int(str_numero[i]);
-        numero = adiciona_elemento_no_inicio(numero, num);
+        numero = adiciona_elemento_no_fim(numero, num);
     }
 
     return numero;
 }
 
-Lista soma() {
-    Lista resultado;
-    resultado = inicia();
+PLista soma(PLista numero1, PLista numero2) {
+    PNo num1;
+    PNo num2;
+    int soma;
+    int adicional = 0;
+    PLista resultado;
 
-    printf("operacao de soma\n");
+    resultado = cria_lista();
+    num1 = numero1->fim;
+    num2 = numero2->fim;
 
-    return resultado;
+    while(num1 != NULL && num2 != NULL) {
+        soma = num1->dado + num2->dado + adicional;
+        if(soma > 9) {
+            soma = soma - 10;
+            adicional = 1;
+        } else {
+            adicional = 0;
+        }
+        resultado = adiciona_elemento_no_inicio(resultado, soma);
+        num1 = num1->anterior;
+        num2 = num2->anterior;
+    }
+
+    if(num1 == NULL && num2 != NULL) {
+        while(num2 != NULL) {
+            soma = num2->dado + adicional;
+            resultado = adiciona_elemento_no_inicio(resultado, soma);
+            num2 = num2->anterior;
+            adicional = 0;
+        }
+        return resultado;
+    } else if(num1 != NULL && num2 == NULL) {
+        while(num1 != NULL) {
+            soma = num1->dado + adicional;
+            resultado = adiciona_elemento_no_inicio(resultado, soma);
+            num1 = num1->anterior;
+            adicional = 0;
+        }
+        return resultado;
+    } else {
+        if(adicional == 1) {
+            resultado = adiciona_elemento_no_inicio(resultado, adicional);
+        }
+        return resultado;
+    }
 }
 
-Lista subtrai() {
-    Lista resultado;
-    resultado = inicia();
+PLista subtrai() {
+    PLista resultado;
+    resultado = cria_lista();
 
     printf("operacao de subtracao\n");
 
     return resultado;
 }
 
-Lista multiplica() {
-    Lista resultado;
-    resultado = inicia();
+PLista multiplica() {
+    PLista resultado;
+    resultado = cria_lista();
 
     printf("operacao de multiplicao\n");
 
     return resultado;
 }
 
-Lista divide() {
-    Lista resultado;
-    resultado = inicia();
+PLista divide() {
+    PLista resultado;
+    resultado = cria_lista();
 
     printf("operacao de divisao\n");
 
     return resultado;
 }
 
-void realiza_operacao(char op) {
-    Lista resultado;
+void realiza_operacao(char op, PLista numero1, PLista numero2) {
+    PLista resultado;
 
     switch(op) {
     case '+':
-        resultado = soma();
-        imprime_resultado(resultado);
+        resultado = soma(numero1, numero2);
+        imprime_lista(resultado);
+        destroi_lista(resultado);
         break;
     case '-':
         resultado = subtrai();
-        imprime_resultado(resultado);
+        imprime_lista(resultado);
+        destroi_lista(resultado);
         break;
     case '*':
         resultado = multiplica();
-        imprime_resultado(resultado);
+        imprime_lista(resultado);
+        destroi_lista(resultado);
         break;
     case '/':
         resultado = divide();
-        imprime_resultado(resultado);
+        imprime_lista(resultado);
+        destroi_lista(resultado);
         break;
     default:
         printf("operacao desconhecida\n");
@@ -211,30 +254,24 @@ int main() {
     PLista numero1;
     PLista numero2;
 
-    numero1 = cria_lista();
-    numero2 = cria_lista();
-
     le_int(&qtd_teste);
-    le_char(&operacao);
-    le_string(str_numero1);
-    le_string(str_numero2);
 
-    numero1 = converte_string_para_lista_int(numero1, str_numero1);
-    numero2 = converte_string_para_lista_int(numero2, str_numero2);
+    for(int i = 0; i < qtd_teste; i++) {
+        le_char(&operacao);
+        le_string(str_numero1);
+        le_string(str_numero2);
 
-    printf("%d ", qtd_teste);
-    printf("%c\n", operacao);
+        numero1 = cria_lista();
+        numero2 = cria_lista();
 
-    /* printf("%s\n", str_numero1); */
-    /* printf("%s\n", str_numero2); */
+        numero1 = converte_string_para_lista_int(numero1, str_numero1);
+        numero2 = converte_string_para_lista_int(numero2, str_numero2);
 
-    imprime_lista(numero1);
-    imprime_lista(numero2);
+        realiza_operacao(operacao, numero1, numero2);
 
-    realiza_operacao(operacao);
-
-    destroi_lista(numero1);
-    destroi_lista(numero2);
+        destroi_lista(numero1);
+        destroi_lista(numero2);
+    }
 
     return 0;
 }
