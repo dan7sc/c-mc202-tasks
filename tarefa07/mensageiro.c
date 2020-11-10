bc2b886b39a49d7aa2bb214c8c9b8f4ba0aaf643
dan7sc <dan7sc@users.noreply.github.com>
2020-11-10 15:48:20 +0000

    cria loop para leitura e insercao de dados

--- tarefa07/mensageiro.c
+++ tarefa07/mensageiro.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include "arvore.h"
 
-void le_int(int *num) {
-    scanf("%d", num);
+int le_int(int *num) {
+    return scanf("%d", num);
 }
 
 void le_char(char *ch) {
@@ -11,37 +11,37 @@ void le_char(char *ch) {
 }
 
 // le string entre aspas e nao armazena as aspas
-/* void le_string_entre_aspas(char *str) { */
-/*     char ch; */
-/*     int i; */
-/*     int contador = 0; */
-
-/*     for(i = 0; contador < 2 && i < TAM_STR - 1; i++) { */
-/*         scanf("%c", &ch); */
-/*         if(ch != '"') { */
-/*             str[i] = ch; */
-/*         } else { */
-/*             i--; */
-/*             contador++; */
-/*         } */
-/*     } */
-/*     str[i] = '\0'; */
-/* } */
-
 void le_string_entre_aspas(char *str) {
     char ch;
     int i;
     int contador = 0;
 
-    for(i = 0; contador < 2 && i < TAM_STR; i++) {
+    for(i = 0; contador < 2 && i < TAM_STR - 1; i++) {
         scanf("%c", &ch);
-        if(ch == '"') {
+        if(ch != '"') {
+            str[i] = ch;
+        } else {
+            i--;
             contador++;
         }
-        str[i] = ch;
     }
     str[i] = '\0';
 }
+
+/* void le_string_entre_aspas(char *str) { */
+/*     char ch; */
+/*     int i; */
+/*     int contador = 0; */
+
+/*     for(i = 0; contador < 2 && i < TAM_STR; i++) { */
+/*         scanf("%c", &ch); */
+/*         if(ch == '"') { */
+/*             contador++; */
+/*         } */
+/*         str[i] = ch; */
+/*     } */
+/*     str[i] = '\0'; */
+/* } */
 
 int compara_cartao_numero(void *numero, void *cartao) {
     int *num = (int *) numero;
@@ -80,36 +80,50 @@ void imprime_autoridade(void *autoridade) {
 int main() {
     int num_cartoes;
     int num_autoridades;
-    /* char ch; */
+    int n;
     Cartao *cartao;
     Autoridade *autoridade;
     Arvore pares;
     Arvore autoridades;
 
-    le_int(&num_cartoes);
-    le_int(&num_autoridades);
+    n = 1;
+    while(n > -1) {
+        while(n > 0) {
+            le_int(&num_cartoes);
+            le_int(&num_autoridades);
 
-    pares = cria_arvore();
-    autoridades = cria_arvore();
+            if(n > 0) {
+                pares = cria_arvore();
+            }
+            for(int i = 0; i < num_cartoes; i++) {
+                cartao = malloc(sizeof(Cartao));
+                le_int(&cartao->numero);
+                le_string_entre_aspas(cartao->texto);
+                if(n > 0) {
+                    pares = insere(pares, cartao, compara_cartao_numero);
+                }
+            }
 
-    for(int i = 0; i < num_cartoes; i++) {
-        cartao = malloc(sizeof(Cartao));
-        le_int(&cartao->numero);
-        le_string_entre_aspas(cartao->texto);
-        pares = insere(pares, cartao, compara_cartao_numero);
+            if(n > 0) {
+                autoridades = cria_arvore();
+            }
+            for(int i = 0; i < num_autoridades; i++) {
+                autoridade = malloc(sizeof(Autoridade));
+                n = le_int(&autoridade->numero);
+                if(n > 0) {
+                    autoridades = insere(autoridades, autoridade, compara_autoridade_numero);
+                }
+            }
+            if(n > 0) {
+                printf("%d %d\n", num_cartoes, num_autoridades);
+                percorre(pares, pre_ordem, imprime_cartao);
+                percorre(autoridades, pre_ordem, imprime_autoridade);
+            }
+
+            destroi_arvore(pares);
+            destroi_arvore(autoridades);
+        }
     }
-
-    for(int i = 0; i < num_autoridades; i++) {
-        autoridade = malloc(sizeof(Autoridade));
-        le_int(&autoridade->numero);
-        autoridades = insere(autoridades, autoridade, compara_autoridade_numero);
-    }
-
-    percorre(pares, pre_ordem, imprime_cartao);
-    percorre(autoridades, pre_ordem, imprime_autoridade);
-
-    destroi_arvore(pares);
-    destroi_arvore(autoridades);
 
     return 0;
 }
