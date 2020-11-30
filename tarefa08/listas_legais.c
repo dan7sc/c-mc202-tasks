346c40829fa0b4c3c29ad23ec4ca308e013d69d8
dan7sc <dan7sc@users.noreply.github.com>
2020-11-30 20:25:31 -0300

    realiza operacao de inserir numero na lista

--- tarefa08/listas_legais.c
+++ tarefa08/listas_legais.c
@@ -8,6 +8,7 @@ int main() {
     int qtde_inicial_lista = 0;
     int num_operacoes = 0;
     int numero = 0;
+    int operacao = 0;
     Arvore lista;
 
     lista = cria_arvore();
@@ -20,7 +21,23 @@ int main() {
         lista = insere(lista, numero);
     }
 
-    printf("\n%d %d\n", qtde_inicial_lista, num_operacoes);
+    for(int i = 0; i < num_operacoes; i++) {
+        le_int(&operacao);
+        switch(operacao) {
+        case 1:
+            le_int(&numero);
+            lista = insere(lista, numero);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+        }
+    }
+
+    printf("%d %d\n", qtde_inicial_lista, num_operacoes);
 
     percorre(lista, pre_ordem);
     percorre(lista, in_ordem);
