f351278f7c8cc386961e506dd0bf51e2e15b39a1
dan7sc <dan7sc@users.noreply.github.com>
2020-12-04 12:48:13 -0300

    adiciona funcoes relacionadas a impressao do relatorio

--- tarefa09/diadeuber.c
+++ tarefa09/diadeuber.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "heap.h"
 
 #define N_ACAO 250
@@ -11,12 +12,35 @@ void copia_cliente(Cliente *ca, Cliente *cb) {
     ca->destino = cb->destino;
 }
 
+int calcula_distancia(Posicao origem, Posicao destino) {
+    return (abs(origem.x - destino.x) + abs(origem.y - destino.y));
+}
+
+double calcula_rendimento_bruto(int n_cancelamentos, int km_rodados) {
+    return (7*n_cancelamentos + 1.4*km_rodados);
+}
+
+double calcula_despesas(int km_rodados) {
+    return (57 + (4.104*km_rodados)/10);
+}
+
+double calcula_rendimento_liquido(double rbruto, double despesas) {
+    return (0.75*rbruto - despesas);
+}
+
 int main() {
     PHeap fila;
     char acao;
     Cliente cliente;
     Cliente *cliente_em_atendimento = NULL;
     Cliente *cliente_cancelado = NULL;
+    int km_rodados = 0;
+    int km_rodados_pagos = 0;
+    int n_cancelamentos = 0;
+    Posicao atual;
+
+    atual.x = 0;
+    atual.y = 0;
 
     fila = cria_heap(N_ACAO);
 
@@ -37,6 +61,11 @@ int main() {
             printf("Cliente %s foi adicionado(a)\n", cliente.nome);
             break;
         case 'F':
+            km_rodados += calcula_distancia(atual, cliente_em_atendimento->origem);
+            atual = cliente_em_atendimento->origem;
+            km_rodados += calcula_distancia(atual, cliente_em_atendimento->destino);
+            km_rodados_pagos += calcula_distancia(atual, cliente_em_atendimento->destino);
+            atual = cliente_em_atendimento->destino;
             printf("A corrida de %s foi finalizada\n", cliente_em_atendimento->nome);
             free(cliente_em_atendimento);
             cliente_em_atendimento = remove_max(fila);
@@ -45,11 +74,18 @@ int main() {
             scanf("%s", cliente.nome);
             maximiza_prioridade(fila, cliente.nome);
             cliente_cancelado = remove_max(fila);
+            n_cancelamentos++;
             printf("%s cancelou a corrida\n", cliente_cancelado->nome);
             free(cliente_cancelado);
             break;
         case 'T':
             printf("\nJornada finalizada. Aqui esta o seu rendimento de hoje\n");
+            printf("Km total: %d\n", km_rodados);
+            printf("Rendimento bruto: %.2lf\n", calcula_rendimento_bruto(n_cancelamentos, km_rodados_pagos));
+            printf("Despesas: %.2lf\n", calcula_despesas(km_rodados));
+            printf("Rendimento liquido: %.2lf\n", calcula_rendimento_liquido(
+                       calcula_rendimento_bruto(n_cancelamentos, km_rodados_pagos),
+                       calcula_despesas(km_rodados)));
             break;
         }
     } while(acao != 'T');
