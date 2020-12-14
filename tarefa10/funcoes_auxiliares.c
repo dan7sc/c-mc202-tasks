2402a249d6225be0d10d5fe8ce9c0da1b7c56efc
dan7sc <dan7sc@users.noreply.github.com>
2020-12-14 10:28:20 -0300

    remove funcao verifica_verossimilhanca e modifica condicionais nas funcoes de verificacao

--- tarefa10/funcoes_auxiliares.c
+++ tarefa10/funcoes_auxiliares.c
@@ -67,31 +67,6 @@ Boolean contem_substring(char *string, char *substring) {
     return FALSE;
 }
 
-Boolean verifica_verossimilhanca(char *string_a, char *string_b) {
-    int contador = 0;
-    int total = 0;
-    char substring[N_CHAR];
-    int tam_sub = 1;
-    int tam_str_a = strlen(string_a);
-
-    for(tam_sub = 1; tam_str_a - tam_sub - 1 > 0; tam_sub++) {
-        for(int k = 0; string_a[k] != '\0' && k < (tam_str_a - tam_sub + 1); k++) {
-            inicializa_string(substring, tam_sub);
-            gera_substring(string_b, k, substring, tam_sub);
-            if(contem_substring(string_a, substring)) {
-                contador++;
-            }
-            total++;
-        }
-    }
-
-    if((200 * contador / total) > 70) {
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
 int verifica_caracteres_faltantes(char *valor, char *palavra) {
     int contador = 0;
     int eh_igual = 0;
@@ -112,11 +87,15 @@ int verifica_caracteres_faltantes(char *valor, char *palavra) {
     }
     palavra_gerada[i] = '\0';
 
-    if(strcmp(palavra_gerada, valor) != 0) {
-        return -1;
+    if((dif > 1 && strcmp(palavra_gerada, valor) == 0) || strcmp(palavra_gerada, valor) != 0) {
+        return 2;
     }
 
-    return dif > 1 ? 0 : 1;
+    if(dif < 1 && strcmp(palavra_gerada, valor) == 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int verifica_caracteres_excedentes(char *valor, char *palavra) {
@@ -126,7 +105,7 @@ int verifica_caracteres_excedentes(char *valor, char *palavra) {
     char palavra_gerada[N_CHAR];
     int i, j;
 
-    for(i = 0, j = 0; valor[i] != '\0'; i++, j++) {
+    for(i = 0, j = 0; palavra[i] != '\0'; i++, j++) {
         eh_igual = compara_caracteres(valor[i], palavra[j]);
         if(eh_igual) {
             contador++;
@@ -139,11 +118,15 @@ int verifica_caracteres_excedentes(char *valor, char *palavra) {
     }
     palavra_gerada[i] = '\0';
 
-    if(strcmp(palavra_gerada, valor) != 0) {
-        return -1;
+    if((dif > 1 && strcmp(palavra_gerada, valor) == 0) || strcmp(palavra_gerada, valor) != 0) {
+        return 2;
     }
 
-    return dif > 1 ? 0 : 1;
+    if(dif < 1 && strcmp(palavra_gerada, valor) == 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int verifica_caracteres_trocados(char *valor, char *palavra) {
@@ -165,19 +148,22 @@ int verifica_caracteres_trocados(char *valor, char *palavra) {
     }
     palavra_gerada[i] = '\0';
 
-    if(strcmp(palavra_gerada, valor) != 0) {
-        return -1;
+    if((dif > 1 && strcmp(palavra_gerada, valor) == 0) || strcmp(palavra_gerada, valor) != 0) {
+        return 2;
     }
 
-    return dif > 1 ? 0 : 1;
+    if(dif < 1 && strcmp(palavra_gerada, valor) == 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 void verifica_palavra_no_dicionario(PHash hash, char *palavra) {
-    int cor = -1;
+    int cor = 2;
     char *temp = NULL;
     int tamanho_palavra = 0;
     int tamanho_valor = 0;
-    Boolean eh_verossimel = FALSE;
 
     temp = busca(hash, palavra);
     if(temp) {
@@ -186,11 +172,10 @@ void verifica_palavra_no_dicionario(PHash hash, char *palavra) {
     temp = NULL;
 
     tamanho_palavra = strlen(palavra);
-    for(int i = 0; i < hash->tamanho && cor < 0; i++) {
+    for(int i = 0; i < hash->tamanho && cor > 1; i++) {
         if(*hash->tabela[i].valor != NULL) {
             tamanho_valor = strlen(*hash->tabela[i].valor);
-            eh_verossimel = verifica_verossimilhanca(*hash->tabela[i].valor, palavra);
-            if(eh_verossimel && abs(tamanho_palavra - tamanho_valor) < 2 ) {
+            if(abs(tamanho_palavra - tamanho_valor) < 2) {
                 if(tamanho_valor > tamanho_palavra) {
                     cor = verifica_caracteres_faltantes(*hash->tabela[i].valor, palavra);
                 } else if(tamanho_valor < tamanho_palavra) {
@@ -202,11 +187,11 @@ void verifica_palavra_no_dicionario(PHash hash, char *palavra) {
         }
     }
 
-    if(cor < 1) {
-        printf("vermelho\n");
-    } else if(cor == 1) {
+    if(cor == 1) {
         printf("amarelo\n");
-    } else {
+    } else if(cor == 0) {
         printf("verde\n");
+    } else {
+        printf("vermelho\n");
     }
 }
