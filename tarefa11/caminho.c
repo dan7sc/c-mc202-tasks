4228e23d54d0eb556625c3433e64378d9108e626
dan7sc <dan7sc@users.noreply.github.com>
2020-12-22 14:48:44 -0300

    busca a maior aresta das menores arestas com um Lugia no grafo

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -1,5 +1,8 @@
-#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "grafo.h"
+#include <math.h>
 
 void imprime_adjacencia(void *dado) {
     Vertice v = *(Vertice *) dado;
@@ -8,35 +11,67 @@ void imprime_adjacencia(void *dado) {
 }
 
 Ponto converte_string_para_enum(char *str) {
-    Ponto ponto = 0;
-    char *strings[] = { "Red", "pokestop", "Lugia" };
+    char *strings[] = { "pokestop", "Lugia" };
 
-    for(int i = 0; i < 3; i++) {
-        if(strcmp(str, strings[i])) {
-            ponto = i;
-        }
+    if(strcmp(str, strings[1]) == 0) {
+        return Lugia;
     }
+    return pokestop;
+}
 
-    return ponto;
+float calcula_distancia(Posicao p1, Posicao p2) {
+    float distancia = 0.0;
+
+    distancia = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+
+    return distancia;
+}
+
+void calcula_distancias_vertice_adjacencia(PVertice v) {
+    PNo atual;
+    PVertice u;
+    float d;
+
+    for(atual = v->adjacencia; atual != NULL; atual = atual->proximo) {
+        u = (PVertice)atual->dado;
+        d = calcula_distancia(v->info.posicao, u->info.posicao);
+        printf("%.2f ", d);
+    }
+    printf("\n");
 }
 
 int main() {
     PGrafo g = cria_grafo();
     Vertice v;
-    int para_de_ler_entrada = 1;
+    PVertice raiz, destino;
+    int para_de_ler_entrada;
     char ponto[8];
+    Posicao origem;
+    int maior_aresta = 0;
 
-    scanf("%f %f", &v.info.posicao.x, &v.info.posicao.y);
-    v.info.ponto = Red;
-    insere_vertice(g, v);
-
+    scanf("%f %f", &origem.x, &origem.y);
+    para_de_ler_entrada = scanf("%f %f %s", &v.info.posicao.x, &v.info.posicao.y, ponto);
     while(para_de_ler_entrada > -1) {
-        para_de_ler_entrada = scanf("%f %f %s", &v.info.posicao.x, &v.info.posicao.y, ponto);
         v.info.ponto = converte_string_para_enum(ponto);
         insere_vertice(g, v);
+        para_de_ler_entrada = scanf("%f %f %s", &v.info.posicao.x, &v.info.posicao.y, ponto);
     }
 
-    imprime_vertices(g, imprime_adjacencia);
+    raiz = g->vertice;
+    while(raiz->info.posicao.x != origem.x && raiz->info.posicao.y != origem.y) {
+        raiz = raiz->proximo;
+    }
+
+    destino = g->vertice;
+    while(destino->info.ponto != Lugia) {
+        destino = destino->proximo;
+    }
+
+    maior_aresta = bfs(g, raiz, destino);
+    printf("%d\n", maior_aresta);
+
+    /* imprime_vertices(g, imprime_adjacencia); */
+    /* calcula_distancias_vertice_adjacencia(g->vertice); */
 
     destroi_grafo(g);
 
