9756cae652f4aeb02bed019605eef3a2876351f2
dan7sc <dan7sc@users.noreply.github.com>
2021-01-13 13:20:12 -0300

    le entrada com numeros inteiros apenas

--- tarefa12/planilha.c
+++ tarefa12/planilha.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("ola mundo\n");
+    char *nome_arquivo = NULL;
+    FILE *arquivo = NULL;
+    int r;
+    int ncol, nlin;
+    int i = 0;
+
+    scanf("%ms ", &nome_arquivo);
+    scanf("%d %d", &ncol, &nlin);
+    printf("%s %d %d\n", nome_arquivo, ncol, nlin);
+
+    arquivo = fopen(nome_arquivo, "r");
+
+    while(fscanf(arquivo, " %d ,", &r) != EOF){
+        printf("%5d ", r);
+        if(i == ncol - 1)
+            printf("\n");
+        i++;
+        if(i == ncol)
+            i = 0;
+    }
+
+    fclose(arquivo);
 
     return 0;
 }
